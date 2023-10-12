8a784e6646805248fe04f9041f5e08f2e8fca6e6
Jeremy <encaladajeremy@gmail.com>
2023-10-12 16:06:01 -0500

    x2

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -10,40 +10,33 @@ const useGame = () => {
 
   useEffect(() => {
     if (game) return
-    console.log('Conectando al servidor desde el frontend')
     socket.on('connect', () => {
       console.log('Conectado al servidor')
     })
 
     socket.on('game-created', (game) => {
-      console.log('Juego creado', game)
       setPlayerLeave(false)
       setGame(game)
     })
 
     socket.on('game-joined', (game) => {
-      console.log('Juego unido', game)
       setGame(game)
     })
 
     socket.on('piece-added', (game) => {
-      console.log('Pieza agregada', game)
       setGame(game)
     })
 
     socket.on('winner-checked', (winner) => {
-      console.log('Ganador verificado', winner)
       setWinner(winner)
     })
 
     socket.on('player-disconnected', () => {
-      console.log('Jugador abandono la partida')
       setPlayerLeave(true)
       setGame(null)
     })
 
     socket.on('tie', () => {
-      console.log('Empate')
       setWinner('tie')
     })
   }, [])
@@ -53,7 +46,6 @@ const useGame = () => {
   }
 
   const addPiece = ({ gameId, piece }) => {
-    console.log('Agregando pieza', gameId, piece)
     socket.emit('add-piece', gameId, piece)
     socket.emit('check-winner', gameId)
   }
